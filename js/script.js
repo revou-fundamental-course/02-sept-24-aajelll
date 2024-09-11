14add02a2f6d36c50af9f756e52e494cc0603c74
aajelll <azizahsyahruniatulm@gmail.com>
2024-09-11 23:21:05 +0800

    modified: css/styles.css with the design modified: js/script.js for their alert each calculate

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 function calculateArea1() { //LUAS SEGITIGA
   var alas = document.getElementById("alas").value;
   var tinggi = document.getElementById("tinggi").value;
+
+  if (alas == '' || tinggi == ''|| alas.includes('e') || tinggi.includes('e')) {
+    alert('Masukkan Nilai Terlebih Dahulu');
+    return;
+  }
   var hasilLuas = 0.5 * alas * tinggi; //rumus
+
   document.getElementById("result1").innerHTML = //menambahkan text pada html untuk hasilnya 
     "Luas Segitiga: 1/2 x " + alas + " x " + tinggi + "<br>" +
     "Luas Segitiga: " + hasilLuas;
@@ -11,7 +17,13 @@ function calculateArea2() { //KELILING SEGITIGA
   var sisi1 = parseFloat (document.getElementById("sisi1").value); //mengambil nilai dari input pada sisi1
   var sisi2 = parseFloat (document.getElementById("sisi2").value);
   var sisi3 = parseFloat (document.getElementById("sisi3").value);
+
+  if (sisi1 == '' || sisi2 == '' || sisi3 == '' || sisi1.includes('e') || sisi2.includes('e') || sisi3.includes('e')) {
+    alert('Masukkan Nilai Terlebih Dahulu');
+    return;
+  }
   var hasilKeliling = sisi1 + sisi2 + sisi3;
+  
   document.getElementById("result2").innerHTML =
     "Keliling Segitiga: " + sisi1 + " + " + sisi2 + " + " + sisi3 + "<br>" +
     "Keliling Segitiga: " + hasilKeliling;
